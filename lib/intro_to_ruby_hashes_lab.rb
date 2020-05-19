6a518bbce73031861672eaa6f83e4bc3271da100
Berel Levy <berellevy@gmail.com>
2020-05-19 16:44:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,11 +7,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-    hash.default = 1
     if hash[key]
         hash[key] += 1
     else
-        hash[key]
+        hash[key] = 1
     end
     return hash
 end
